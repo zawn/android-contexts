747af7bac699693ac4206362ef351832a54e1ec2
zhangzhenli <zhangzhenli@live.com>
2019-08-28 15:12:53 +0800

    android mock api

---
+++ android-mock/src/main/java/android/app/ActivityThread.java
@@ -0,0 +1,24 @@
+package android.app;
+
+public final class ActivityThread {
+
+    public static ActivityThread currentActivityThread() {
+        throw new UnsupportedOperationException("stub");
+    }
+
+    public static String currentOpPackageName() {
+        throw new UnsupportedOperationException("stub");
+    }
+
+    public static String currentPackageName() {
+        throw new UnsupportedOperationException("stub");
+    }
+
+    public static String currentProcessName() {
+        throw new UnsupportedOperationException("stub");
+    }
+
+    public static Application currentApplication() {
+        throw new UnsupportedOperationException("stub");
+    }
+}
